33edd31501238e39203b43ce37af4c82d188b279
sumancfc <sumanstha999@gmail.com>
2026-07-19 15:45:08 +0200

    fix: restore build by avoiding server-side i18n in layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,19 +4,16 @@ import menus from "../data/menus";
 import Link from "next/link";
 import FloatingContact from "./FloatingContact";
 import PageBackground from "./PageBackground";
-import i18n from "../lib/i18n";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const skipLinkLabel = i18n.t("skip_to_content", { defaultValue: "Zum Inhalt springen" });
-
   return (
     <div className='page-wrapper' data-testid='page-wrapper'>
       <PageBackground />
-      <Link href="#main-content" className="absolute skip-link">{skipLinkLabel}</Link>
+      <Link href="#main-content" className="absolute skip-link">Zum Inhalt springen</Link>
       <NavDock menus={menus} />
       <main id="main-content" tabIndex={-1}>
         <FloatingContact />
